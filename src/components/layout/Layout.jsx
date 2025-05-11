52c8cef07f8fb1d9ad7f9378e9b4cb375f17c039
hermantoXYZ <andiupdates@gmail.com>
2025-05-11 15:14:58 +0800

    p

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,16 +1,23 @@
 import Navbar from "@/components/layout/Navbar"
 import Footer from '@/components/layout/Footer'
+import { useLocation } from "react-router-dom";
+
 
 const Layout = ({ children }) => {
+
+  const location = useLocation();
+  const isAuthPage = ['/login', '/register'].includes(location.pathname);
+
   return (
     <div className="min-h-screen flex flex-col">
-      <Navbar />
-      <main className="flex-grow relative z-0">
+      {!isAuthPage && <Navbar />}
+      <main className="flex-grow">
         {children}
       </main>
-      <Footer />
+      {!isAuthPage && <Footer />}
     </div>
   );
 };
+
 
 export default Layout;
